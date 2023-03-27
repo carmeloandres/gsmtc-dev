95a007f1a254f135eb029c58d69a614c8fa9cea1
carmelo <carmeload@gmail.com>
2023-03-27 16:24:20 +0200

    starting

--- inc/class-gsmtc-developer.php
+++ inc/class-gsmtc-developer.php
@@ -10,6 +10,7 @@ class Gsmtc_Developer{
 
 		add_action('wp_enqueue_scripts',array($this,'wp_enqueue_scripts'));
         add_action('after_setup_theme',array($this,'after_setup_theme'));
+        add_filter('render_block',array($this,'render_block'),10,3);
 /*        add_action('init',array($this,'init'));
 		add_filter('login_redirect',array($this,'redirect_restaurador'),10,3);
 		add_filter( 'page_template',array($this,'hide_admin_bar') );
@@ -21,6 +22,36 @@ class Gsmtc_Developer{
 		add_filter('pre_get_document_title',array($this,'titulo_pestanya'));
 */
 	}
+
+    function render_block($block_content, $block, $instance){
+
+        if (
+            $block['blockName'] === 'core/navigation' && 
+            !is_admin() &&
+            !wp_is_json_request()
+            ) {
+
+            $new_block = preg_replace('/<svg.*?\/svg>/i','<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"/></svg>', $block_content);
+
+//            $new_block = preg_replace('/\<svg width(.*?)\<\/svg\>/', "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'><path stroke='rgba%280,0,0,0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/></svg>", $block_content);
+            error_log ('La función "render_block" ha sido ejecutada, $block_content: '.var_export($new_block,true).PHP_EOL);
+
+            return $new_block;
+//            return preg_replace('/\<svg width(.*?)\<\/svg\>/', "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'><path stroke='rgba%280,0,0,0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/></svg>", $block_content);
+        } 
+        
+        if (
+            $block['blockName'] === 'core/navigation' && 
+            !is_admin() &&
+            !wp_is_json_request()
+            ) {
+                error_log ('La función "render_block" ha sido ejecutada, $block: '.var_export($block,true).PHP_EOL);
+                
+            }
+        
+    
+        return $block_content;
+    }
 
 	/**
 	 * Esta función carga los estilos y scripts necesarias para el tema
