4e5fc2542c8af27aeef6086528ce80340b3ca46f
carmeloandres <carmeload@gmail.com>
2024-04-25 16:53:56 +0200

    performing some modifications to the 'functions' file

--- inc/class-gsmtc-developer.php
+++ inc/class-gsmtc-developer.php
@@ -11,16 +11,6 @@ class Gsmtc_Developer{
 		add_action('wp_enqueue_scripts',array($this,'wp_enqueue_scripts'));
         add_action('after_setup_theme',array($this,'after_setup_theme'));
         add_filter('render_block',array($this,'render_block'),10,3);
-/*        add_action('init',array($this,'init'));
-		add_filter('login_redirect',array($this,'redirect_restaurador'),10,3);
-		add_filter( 'page_template',array($this,'hide_admin_bar') );
-		add_action('switch_theme',array($this,'borrar_pagina_restaurador'));
-		add_action('init',array($this,'reescritura_de_urls'));
-		add_filter( 'query_vars', array($this,'mis_query_vars'));
-		add_action('after_setup_theme',array($this,'nuevas_dimensiones_imagen'));
-//		add_filter('wp_title',array($this,'titulo_pestanya'),10,2);
-		add_filter('pre_get_document_title',array($this,'titulo_pestanya'));
-*/
 	}
 
 	/**
@@ -41,7 +31,6 @@ class Gsmtc_Developer{
                 
             $new_block = preg_replace('/src.*?.png/i', $nueva_imagen, $block_content);
 
-
             return $new_block;
 
         }
@@ -54,8 +43,7 @@ class Gsmtc_Developer{
 
             return $new_block;
         } 
-                
-    
+                   
         return $block_content;
     }
 
@@ -65,12 +53,12 @@ class Gsmtc_Developer{
     function wp_enqueue_scripts(){
 
         wp_enqueue_style(
-            'gsmtc-dev-motnserrat',
+            'gsmtc-dev-montserrat',
             'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap',
             [],
             null
         );
-
+/*
         wp_enqueue_style(
             'gsmtc-dev-bootstrap',
             get_theme_file_uri('assets/css/bootstrap.min.css'),
@@ -80,10 +68,12 @@ class Gsmtc_Developer{
             'gsmtc-dev-bootstrap-icons',
             get_theme_file_uri('assets/bootstrap-icons/bootstrap-icons.css'),
         );
-
+*/
         wp_enqueue_style(
             'gsmtc-dev-theme',
             get_theme_file_uri('style.css'),
+            [],
+            '1'
         );
 
     }
@@ -95,9 +85,5 @@ class Gsmtc_Developer{
         add_theme_support('editor-styles');
     }
 
-	/**
-	 * Esta función crea los custom post types y el rol de usuario 'restaurador'
-	 */
-	function init() {
-    }
+
 }
